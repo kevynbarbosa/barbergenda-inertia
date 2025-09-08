9ec79ede6d6f6b3a0fa2300387745f802f189d6a
Kevyn Barbosa <kevynbarbosa1@gmail.com>
2025-09-08 16:11:04 -0300

    Atualizando lucide icons

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -6,7 +6,7 @@ import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, Sid
 import { dashboard } from '@/routes';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
+import { BookOpen, Folder, Inbox, LayoutGrid, ShieldUser } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
@@ -14,6 +14,18 @@ const mainNavItems: NavItem[] = [
         title: 'Dashboard',
         href: dashboard(),
         icon: LayoutGrid,
+    },
+
+    {
+        title: 'Permissões',
+        href: dashboard(),
+        icon: ShieldUser,
+    },
+
+    {
+        title: 'Caixa de entrada',
+        href: dashboard(),
+        icon: Inbox,
     },
 ];
 
