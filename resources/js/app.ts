2d2661836dc2c0e06c15f33db8d9a487f1ac1ff8
Kevyn Barbosa <kevynbarbosa1@gmail.com>
2025-09-10 01:09:51 -0300

    Layout padrao

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -5,12 +5,22 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { initializeTheme } from './composables/useAppearance';
+import AppLayout from './layouts/AppLayout.vue';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
-    resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob<DefineComponent>('./pages/**/*.vue')),
+    resolve: (name) => {
+        const page = resolvePageComponent(`./pages/${name}.vue`, import.meta.glob<DefineComponent>('./pages/**/*.vue'));
+        page.then((module) => {
+            // Don't apply default layout to auth pages and Welcome page
+            if (!name.startsWith('auth/') && name !== 'Welcome') {
+                module.default.layout = module.default.layout || ((h, page) => h(AppLayout, () => page));
+            }
+        });
+        return page;
+    },
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
