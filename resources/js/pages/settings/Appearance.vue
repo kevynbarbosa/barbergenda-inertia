e74be60aabcb14bd1afa95c744edf71e401c0475
Kevyn Barbosa <kevynbarbosa1@gmail.com>
2025-09-17 17:37:09 -0300

    Removendo layout duplicado em settings

--- resources/js/pages/settings/Appearance.vue
+++ resources/js/pages/settings/Appearance.vue
@@ -3,29 +3,16 @@ import { Head } from '@inertiajs/vue3';
 
 import AppearanceTabs from '@/components/AppearanceTabs.vue';
 import HeadingSmall from '@/components/HeadingSmall.vue';
-import { type BreadcrumbItem } from '@/types';
-
-import AppLayout from '@/layouts/AppLayout.vue';
 import SettingsLayout from '@/layouts/settings/Layout.vue';
-import { appearance } from '@/routes';
-
-const breadcrumbItems: BreadcrumbItem[] = [
-    {
-        title: 'Appearance settings',
-        href: appearance().url,
-    },
-];
 </script>
 
 <template>
-    <AppLayout :breadcrumbs="breadcrumbItems">
-        <Head title="Appearance settings" />
+    <Head title="Appearance settings" />
 
-        <SettingsLayout>
-            <div class="space-y-6">
-                <HeadingSmall title="Appearance settings" description="Update your account's appearance settings" />
-                <AppearanceTabs />
-            </div>
-        </SettingsLayout>
-    </AppLayout>
+    <SettingsLayout>
+        <div class="space-y-6">
+            <HeadingSmall title="Appearance settings" description="Update your account's appearance settings" />
+            <AppearanceTabs />
+        </div>
+    </SettingsLayout>
 </template>
